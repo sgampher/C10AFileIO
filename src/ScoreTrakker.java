db6d105fb2a290bab74eb05b91f70f888f055a91
sgampher <saragampher@gmail.com>
2024-09-25 03:58:18 -0600

    project implementation

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,4 +1,74 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class ScoreTrakker {
+    private ArrayList<Student> studentlist = new ArrayList<>();
+    private String[] filelist = {"scores.txt", "badscore.txt", "nofile.txt"};
 
+    /*
+     * loadDataFile: takes filename as a parameter, then uses filename to specify file to read in student data
+     * then stores student objects in the studentlist ArrayList.
+
+     */
+    public void loadDataFile(String inFile) {
+        FileReader reader = null;
+        Scanner in = null;
+        try {
+            reader = new FileReader(inFile);
+            in = new Scanner(reader);
+        } catch (FileNotFoundException e) {
+            System.out.println("Can't open file: " + inFile);
+            return; 
+        }
+
+        String name = "";
+        String scoreStr = "";
+        while (in.hasNext()) {
+            try {
+                name = in.nextLine();
+                scoreStr = in.nextLine(); 
+                int score = Integer.parseInt(scoreStr); // implementation found in recommended slack resource
+                studentlist.add(new Student(name, score)); 
+            } catch (NumberFormatException e) {
+                System.out.println("Incorrect format for " + name + " not a valid score: " + scoreStr + '\n');
+            }
+        }
+
+        in.close();
+    }
+    
+    /*
+     * printInOrder: sorts and prints the studentlist ArrayList.
+     */
+    public void printInOrder() {
+        Collections.sort(studentlist); // Sort using Comparable (Student class should implement Comparable)
+        System.out.println("Student Score List");
+        for (Student student : studentlist) {
+            System.out.println(student);
+        }
+    }
+    
+    /*
+     * processFiles: loops through all files in the filelist array.
+     */
+    public void processFiles() {
+        for (String file : filelist) {
+            studentlist.clear();
+            loadDataFile(file);
+            if (!studentlist.isEmpty()) {
+                printInOrder();
+                System.out.println();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ScoreTrakker trakker = new ScoreTrakker();
+        trakker.processFiles();
+    }
 }
+
+
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,19 @@
+public class Student implements Comparable<Student> {
+    private String name;
+    private int score;
 
-public class Student {
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
 
+    @Override
+    public int compareTo(Student other) {
+        return this.name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + score;
+    }
 }
